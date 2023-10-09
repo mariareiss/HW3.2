b96d6c5a0ea2b73a54a491ccce682003d3578051
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-10-09 10:33:04 -0500

    Update view-customers-purchases.php

--- view-customers-purchases.php
+++ view-customers-purchases.php
@@ -9,14 +9,17 @@ while ($customer = $customers->fetch_assoc()){
     <div class="card-body">
       <h5 class="card-title"><?php echo $customer['customer_name'];?></h5>
       <p class="card-text">
+      <ul class="list-group">
   <?php
     $products= selectProductsByCustomers($customer['customer_id']);
   while ($product = $products->fetch_assoc(){
  ?>
+         <li class="list-group-item"><?php echo $product['product_name'];?> - <?php echo $product['product_price'];?> -  <?php echo $product['order_date'];?> - <?php echo $product['store_name'];?></li>
          
  <?php
   }
   ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Phone: <?php echo $customer['customer_phone'];?></small></p>
     </div>
