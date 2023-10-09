42272336b317478128368179b2894dff6fcc0f29
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-10-09 10:37:12 -0500

    Update view-customers-purchases.php

--- view-customers-purchases.php
+++ view-customers-purchases.php
@@ -1,7 +1,5 @@
 <h1>Customers Purchases</h1>
 <div class="card-group">
-
-
 <?php
 while ($customer = $customers->fetch_assoc()){
 ?>
@@ -11,11 +9,10 @@ while ($customer = $customers->fetch_assoc()){
       <p class="card-text">
       <ul class="list-group">
   <?php
-    $products= selectProductsByCustomers($customer['customer_id']);
-  while ($product = $products->fetch_assoc(){
+  $products= selectProductsByCustomers($customer['customer_id']);
+  while ($product = $products->fetch_assoc()){
  ?>
-         <li class="list-group-item"><?php echo $product['product_name'];?> - <?php echo $product['product_price'];?> -  <?php echo $product['order_date'];?> - <?php echo $product['store_name'];?></li>
-         
+         <li class="list-group-item"><?php echo $product['product_name'];?> - <?php echo $product['product_price'];?> - <?php echo $product['order_date'];?> - <?php echo $product['store_name'];?></li>        
  <?php
   }
   ?>
