3d930bfe954513b1f8de04702d130bf82b1b039a
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-10-08 19:08:33 -0500

    Update view-customers.php

--- view-customers.php
+++ view-customers.php
@@ -7,6 +7,7 @@
       <th>Name</th>
       <th>City</th>
       <th>Phone</th>  
+      <th></th>
       </tr>
     </thead>
     <tbody>
@@ -17,7 +18,8 @@ while ($customer = $customers->fetch_assoc()){
    <td><?php echo $customer['customer_id'];?></td> 
    <td><?php echo $customer['customer_name'];?></td>
    <td><?php echo $customer['customer_city'];?></td>
-   <td><?php echo $customer['customer_phone'];?></td>    
+   <td><?php echo $customer['customer_phone'];?></td>
+   <td><a href="products-by-customers.php?id=<?php echo $customer['customer_id'];?>">Products</a></td>
   </tr>
 <?php
 }  
