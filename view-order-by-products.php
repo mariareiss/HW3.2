d822ee186bb5f7c68cbad0ef6eb64a19b2bc0831
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-10-08 20:34:01 -0500

    Create view-order-by-products.php

---
+++ view-order-by-products.php
@@ -0,0 +1,31 @@
+<h1>Order By Products</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Price</th> 
+      <th>Date</th> 
+      <th>Quantity</th> 
+      <th>Store</th> 
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($order = $orders->fetch_assoc()){
+?>
+  <tr>
+   <td><?php echo $order['product_id'];?></td> 
+   <td><?php echo $order['product_name'];?></td> 
+   <td><?php echo $order['product_price'];?></td>   
+   <td><?php echo $order['order_date'];?></td>   
+   <td><?php echo $order['order_quantity'];?></td>   
+   <td><?php echo $order['store_name'];?></td>   
+  </tr>
+<?php
+}  
+?>
+    </tbody>
+  </table>
+</div>
