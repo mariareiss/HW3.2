f4952668616c75bd6b35dc5a3a583d84a6a37bd2
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-10-25 10:44:03 -0500

    Update shipping.php

--- shipping.php
+++ shipping.php
@@ -4,6 +4,33 @@ require_once("model-shipping.php");
 
 $pageTitle = "Shipping";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']){
+    case "Add":
+    if (insertShipping($_POST['sDate'], $_POST['sProduct'])){
+      echo '<div class="alert alert-success" role="alert">Shipping added.</div>';
+    } else {
+      echo '<div class="alert alert-error" role="alert">Error</div>';      
+    }
+    break;
+    case "Edit":
+    if (updateShipping($_POST['sDate'], $_POST['sProduct'], $_POST['sid'])){
+      echo '<div class="alert alert-success" role="alert">Shipping edited.</div>';
+    } else {
+      echo '<div class="alert alert-error" role="alert">Error</div>';      
+    }
+    break;
+    case "Delete":
+    if (deleteShipping($_POST['sid'])){
+      echo '<div class="alert alert-success" role="alert">Shipping deleted.</div>';
+    } else {
+      echo '<div class="alert alert-error" role="alert">Error</div>';      
+    }
+    break;
+  }
+}
+
 $shipping = selectShipping();
 include "view-shipping.php";
 include "view-footer.php";
