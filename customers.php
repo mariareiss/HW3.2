90ee12f467a7c93090a983a211d278176a730be9
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-10-24 20:54:40 -0500

    Update customers.php

--- customers.php
+++ customers.php
@@ -4,6 +4,31 @@ require_once("model-customers.php");
 
 $pageTitle = "Customers";
 include "view-header.php";
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']){
+    case "Add":
+    if (insertCustomer($_POST['cName'], $_POST['cCity'], $_POST['cPhone')){
+      echo '<div class="alert alert-success" role="alert">Customer added.</div>';
+    } else {
+      echo '<div class="alert alert-error" role="alert">Error</div>';      
+    }
+    break;
+    case "Edit":
+    if (updateCustomer($_POST['cName'], $_POST['cCity'], $_POST['cPhone'], $_POST['cid'])){
+      echo '<div class="alert alert-success" role="alert">Customer edited.</div>';
+    } else {
+      echo '<div class="alert alert-error" role="alert">Error</div>';      
+    }
+    break;
+    case "Delete":
+    if (deleteCustomer($_POST['cid'])){
+      echo '<div class="alert alert-success" role="alert">Customer deleted.</div>';
+    } else {
+      echo '<div class="alert alert-error" role="alert">Error</div>';      
+    }
+    break;
+  }
+}
 $customers = selectCustomers();
 include "view-customers.php";
 include "view-footer.php";
