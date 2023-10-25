31c92f31b26f9a69497d90a1343f5ecce4b4c3ce
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-10-24 19:07:09 -0500

    Update products.php

--- products.php
+++ products.php
@@ -4,6 +4,15 @@ require_once("model-products.php");
 
 $pageTitle = "Products";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']){
+    case "Add":
+    insertProduct($_POST['pName'], $_POST['pPrice']);
+    break;
+  }
+}
+
 $products = selectProducts();
 include "view-products.php";
 include "view-footer.php";
