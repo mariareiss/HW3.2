a35d2266800d3b8601e28288dbac48a741366473
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-10-24 19:14:23 -0500

    Update products.php

--- products.php
+++ products.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']){
     case "Add":
-    insertProduct($_POST['pName'], $_POST['pPrice']);
+    if (insertProduct($_POST['pName'], $_POST['pPrice'])){
+      echo '<div class="alert alert-success" role="alert">Product added.</div>"';
+    } else {
+      echo '<div class="alert alert-error" role="alert">Error</div>"';      
+    }
     break;
   }
 }
